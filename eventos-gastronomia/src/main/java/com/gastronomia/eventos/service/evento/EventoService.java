ba2731bb6224a2c832161794d0e5165e3e723c9d
juanma479 <juanmamk9@gmail.com>
2024-08-23 19:48:31 -0300

    commit: se creo en eventoservice metodo para asignar chef

--- eventos-gastronomia/src/main/java/com/gastronomia/eventos/service/evento/EventoService.java
+++ eventos-gastronomia/src/main/java/com/gastronomia/eventos/service/evento/EventoService.java
@@ -2,11 +2,13 @@ package com.gastronomia.eventos.service.evento;
 
 import java.util.Scanner;
 
+import com.gastronomia.eventos.domain.Chef;
 import com.gastronomia.eventos.domain.Evento;
 import com.gastronomia.eventos.domain.Participante;
 
 public interface EventoService {
     Evento crearEvento(Scanner scanner);
+    void asignarChef(Chef chef, Evento evento);
     void inscribirParticipante(Participante participante, Evento evento);
 
 }
--- eventos-gastronomia/src/main/java/com/gastronomia/eventos/service/evento/impl/EventoServiceImpl.java
+++ eventos-gastronomia/src/main/java/com/gastronomia/eventos/service/evento/impl/EventoServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
+import com.gastronomia.eventos.domain.Chef;
 import com.gastronomia.eventos.domain.Evento;
 import com.gastronomia.eventos.domain.Participante;
 import com.gastronomia.eventos.service.evento.EventoService;
@@ -15,11 +16,10 @@ public class EventoServiceImpl implements EventoService {
     public Evento crearEvento(Scanner scanner) {
         System.out.println("Ingrese el nombre del evento: ");
         String nombreEvento = scanner.nextLine();
-        
 
         System.out.println("Ingrese una descripción del evento: ");
         String descripEvento = scanner.nextLine();
-        
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         LocalDateTime fechaHora = null;
         boolean fechaHoraValida = false;
@@ -36,7 +36,6 @@ public class EventoServiceImpl implements EventoService {
                 System.out.println("Formato de fecha y hora incorrecto. Por favor, intente nuevamente.");
             }
         }
-        
 
         System.out.println("Ingrese donde se realizará el evento: ");
         String ubicacion = scanner.nextLine();
@@ -52,15 +51,31 @@ public class EventoServiceImpl implements EventoService {
     }
 
     @Override
+    public void asignarChef(Chef chef, Evento evento) {
+        if (chef == null || evento == null) {
+            System.out.println("El Chef o evento no puede ser nulo.");
+            return;
+        }
+
+        // Manejo de participante ya inscripto y evento a capacidad máxima
+        if (evento.getChefACargo() == null) {
+            evento.setChefACargo(chef);
+            System.out.println("Chef " + chef.getNombreChef() + " asignado al evento exitosamente.");
+        } else {
+            System.out.println("El evento " + evento.getNombreEvento() + " ya tiene un chef asignado.");
+        }
+    }
+
+    @Override
     public void inscribirParticipante(Participante participante, Evento evento) {
         if (participante == null || evento == null) {
             System.out.println("Participante o evento no puede ser nulo.");
             return;
         }
-    
+
         String nombreCompletoPart = participante.getNombreParticipante() + " " + participante.getApellidoParticipante();
-    
-        //Manejo de participante ya inscripto y evento a capacidad máxima
+
+        // Manejo de participante ya inscripto y evento a capacidad máxima
         if (evento.getParticipantes().containsKey(nombreCompletoPart)) {
             System.out.println("El participante " + nombreCompletoPart + " ya está inscrito en este evento.");
         } else if (evento.getParticipantes().size() == evento.getCapacidad()) {
